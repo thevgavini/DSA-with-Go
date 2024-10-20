66ae6374be5ae1f1a6c58033d9138de933a1f88f
thevgavini <gavinivivekananda@gmail.com>
2024-10-21 00:04:24 +0530

    used type 'bool' for improved efficiency

--- Leetcode/medium/countPrimes.go
+++ Leetcode/medium/countPrimes.go
@@ -1,31 +1,34 @@
 package leetcode
 
 func countPrimes(n int) int {
-
 	if n < 2 {
-	return 0
-}
-var eratoSieve func(int) []int
-eratoSieve = func(n int)[]int {
-arr:= make([]int,n+1)
-//initializing arr with 1s
-for i:=2; i<=n; i++ {
-	arr[i] = 1
-}
-for k:=2;k*k<=n; k++ {
-	if arr[k] == 1{
-		for j:=k*k; j<=n; j+=k {
-			arr[j] = 0
-		}
+		return 0
 	}
 
-}
-return arr
-}
-list:=eratoSieve(n)
-count:=0
-for i:=2; i<n; i++ {
-count+=list[i]
-} 
-return count
+	var eratoSieve func(int) []bool
+	eratoSieve = func(n int) []bool {
+		isPrime := make([]bool, n+1)
+		// initializing arr with 1s
+		for i := 2; i <= n; i++ {
+			isPrime[i] = true
+		}
+
+		for k := 2; k*k <= n; k++ {
+			if isPrime[k] == true {
+				for j := k * k; j <= n; j += k {
+					isPrime[j] = false
+				}
+			}
+		}
+		return isPrime
+	}
+
+	list := eratoSieve(n)
+	count := 0
+	for i := 2; i < n; i++ {
+		if list[i] {
+			count++
+		}
+	} 
+	return count
 }
